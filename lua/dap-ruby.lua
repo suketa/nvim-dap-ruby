6bc9a9b9eb37f6a99b3c4e2ede2fe02d8ed2d9ce
tttffff <tristanfellows@icloud.com>
2024-04-26 08:06:19 +0100

    Add back stdout to repl

--- lua/dap-ruby.lua
+++ lua/dap-ruby.lua
@@ -34,6 +34,7 @@ end
 local function run_cmd(cmd, args, for_current_line, for_current_file)
 	local handle
 	local pid_or_err
+	local stdout = vim.loop.new_pipe(false)
 	local working_dir = find_cmd_dir(cmd)
 	args = args or {}
 	if for_current_line then
@@ -41,7 +42,7 @@ local function run_cmd(cmd, args, for_current_line, for_current_file)
 	elseif for_current_file then
 		table.insert(args, vim.fn.expand("%:p"))
 	end
-	local opts = { args = args, cwd = working_dir }
+	local opts = { args = args, cwd = working_dir, stdio = { nil, stdout } }
 
 	handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
 		if handle then
@@ -54,6 +55,15 @@ local function run_cmd(cmd, args, for_current_line, for_current_file)
 	end)
 
 	assert(handle, "Error running command: " .. cmd .. tostring(pid_or_err))
+
+	stdout:read_start(function(err, chunk)
+		assert(not err, err)
+		if chunk then
+			vim.schedule(function()
+				require("dap.repl").append(chunk)
+			end)
+		end
+	end)
 end
 
 local function setup_ruby_adapter(dap)
@@ -96,7 +106,7 @@ local function setup_ruby_configuration(dap)
 		extend_run_config({ name = "run rspec current file", command = "bundle", args = { "exec", "rspec" }, current_file = true }),
 		extend_run_config({ name = "run rspec current_file:current_line", command = "bundle", args = { "exec", "rspec" }, current_line = true }),
 		extend_run_config({ name = "run rspec", command = "bundle", args = { "exec", "rspec" } }),
-		extend_run_config({ name = "bin/dev", command = "bin/dev", local_command = true }),
+		extend_run_config({ name = "bin/dev", command = "bin/dev" }),
 		extend_base_config({ name = "attach existing (port 38698)", port = 38698, waiting = 0 }),
 		extend_base_config({ name = "attach existing (pick port)", waiting = 0 }),
 	}
